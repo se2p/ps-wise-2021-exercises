50cf469d258a35cfd9a079106295fdd5adb5a3a1
Alessio Gambi <alessio.gambi@uni-passau.de>
2020-11-12 21:27:48 +0100

    Solution Session 01 - Task 02

--- session-01/task-02/word_index.py
+++ session-01/task-02/word_index.py
@@ -3,12 +3,13 @@
 # Program Description:
 #
 # Word Index is a program that takes a plain text file as input and
-# outputs all the words contained in it
-# sorted alphabetically along with the page numbers on which they occur.
-# The program assumes that a page is a
-# sequence of 45 lines, each line has max 80 characters, and there is no
-# hyphenation. Additionally, Word Index
-# must ignore all words that occur more than 100 times.
+# outputs all the words contained in it sorted alphabetically along
+# with the page numbers on which they occur.
+
+# The program assumes that a page is a sequence of 45 lines,
+# each line has max 80 characters, and there is no hyphenation.
+
+# Additionally, Word Index must ignore all words that occur more than 100 times.
 
 ####################################
 # Free Flow, a.k.a. No Style
@@ -22,8 +23,79 @@ MAX_SIZE_LINE        =  80
 STOP_FREQUENCY_LIMIT = 100
 
 # Defining a main method makes testing easier
+class WordIndexData:
+
+    def __init__(self, word):
+        self.word = word
+        self.frequency = 0
+        self.pages = set()
+
+    def appear_on(self, page):
+        """ Increment frequency of occurrences and store page without duplicates"""
+        self.frequency += 1
+        self.pages.add(page)
+
+
+def parse_line(line):
+    """ Parse the line and return the list of words found. We assume line is less or equal to 80 chars"""
+    start_char = None
+    for i, c in enumerate(line):
+
+        if start_char is None:
+            if c.isalnum():
+                # We found the start of a word
+                start_char = i
+        else:
+            if not c.isalnum():
+                # We found the end of a word.
+                word = line[start_char:i].lower()
+                yield word
+
+                # Let's reset and restart
+                start_char = None
+
+
 def main(file_path):
-    pass
+    stop_words = []
+    word_index = []
+
+    current_page = 0
+
+    # In this case, it is not necessary to append the '\n' at the end of the line
+    with open(file_path, 'r') as f:
+
+        for line_number, line in enumerate(f):
+
+            # Compute the current page using the line_index. Pages START from 1
+            if line_number % LINES_PER_PAGE == 0:
+                current_page += 1
+
+            for word in parse_line(line):
+                if word not in [data.word for data in word_index[:]]:
+                    data = WordIndexData(word)
+                    data.appear_on(current_page)
+                    #
+                    word_index.append(data)
+                else:
+                    # Update data
+                    [data.appear_on(current_page) for data in word_index if data.word == word]
+
+    # Remove all the words that appeared too often
+    # Q: Can you find a more python way to rewrite this code?
+    word_index = list(filter(lambda data: data.frequency <= STOP_FREQUENCY_LIMIT, word_index))
+
+    # if data.frequency > :
+    #     stop_words.append(word)
+    #     word_index.remove(data)
+
+    # Sort the word_index by word. This changes the object itself
+    word_index.sort(key=lambda x: x.word, reverse=False)
+
+    # Print them
+    for data in word_index:
+        # Q10: Is this ('[1:-1]') a violation? What does it even mean ?!
+        print(data.word, '-', str(data.pages)[1:-1])
+
 
 if __name__ == "__main__":
     main(sys.argv[1])
--- session-01/task-02/word_index_unit_tests.py
+++ session-01/task-02/word_index_unit_tests.py
@@ -1,16 +1,56 @@
 import unittest
-import sys
+import io
+import inspect
+import os
+
+# See: https://stackoverflow.com/questions/1218933/can-i-redirect-the-stdout-in-python-into-some-sort-of-string-buffer
+from contextlib import redirect_stdout
 
 # Import the 'main function from word_index.py as wi_main
 from word_index import main as wi_main
 
-class TestPythonVersion(unittest.TestCase):
-    """ This is a sample unit test """
+# Find ../test-data. Note there are two os.path.dirname
 
-    def test_python_version(self):
-        """ This is a simple example of a unittest that checks if you are running the right version of Python """
-        self.assertEqual(sys.version_info.major, 3, msg="You are not running python 3")
-        self.assertEqual(sys.version_info.minor, 7, msg="You are not running python 3.7")
+TEST_DATA = os.path.join(os.path.dirname(os.path.dirname(os.path.normpath(os.path.realpath(__file__)))), "test-data")
+
+class TestWordIndex(unittest.TestCase):
+    def test_output_with_an_empty_file_produces_not_output(self):
+        print(inspect.stack()[0][3]) # Print the name of THIS function
+
+        # Execute main and be sure you capture the output
+        with io.StringIO() as buf, redirect_stdout(buf):
+            wi_main(os.path.join(TEST_DATA, './empty.txt'))
+            output = buf.getvalue()
+
+        # Check the output is not None
+        self.assertIsNotNone(output, msg="Output cannot be None")
+
+        # Check the output is not empty
+        self.assertEqual(output, '', msg="Output is not empty")
+
+    def test_output_with_another_one_line_file(self):
+        print(inspect.stack()[0][3])
+
+        # Execute main and be sure you capture the output
+        with io.StringIO() as buf, redirect_stdout(buf):
+            wi_main(os.path.join(TEST_DATA, './one-line.txt'))
+            output = buf.getvalue()
+
+        # Check the output is the expected one.
+        # Q: Do we care about the last '\n' ?
+        self.assertEqual(output, "blablba - 1\n", msg="Output cannot be empty")
+
+    def test_frequent_word_is_filtered(self):
+        print(inspect.stack()[0][3])
+
+        # Execute main and be sure you capture the output
+        with io.StringIO() as buf, redirect_stdout(buf):
+            wi_main(os.path.join(TEST_DATA, './test.txt'))
+            output = buf.getvalue()
+
+        # Check the output is the expected one.
+        # https://kapeli.com/cheat_sheets/Python_unittest_Assertions.docset/Contents/Resources/Documents/index
+        self.assertNotIn("Line", output, msg="Output contains filtered word")
 
 if __name__ == '__main__':
     unittest.main()
