19cfd3de5d5dc572a4e9fb89ccd4373e07a8c070
Alessio Gambi <alessio.gambi@uni-passau.de>
2020-11-26 13:55:30 +0100

    Fixed error in word_index.py. Implemented three unit tests

--- session-02/task-01/word_index.py
+++ session-02/task-01/word_index.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import sys
 
 # Monolithic Style Description:
 #
@@ -8,16 +9,131 @@
 
 # Global Constants:
 #   Q1: Can those be considered as abstractions? Of which type?
-#   Q2: Do they violate the style?
-LINES_PER_PAGE = 45
-MAX_SIZE_LINE = 80
-STOP_FREQUENCY_LIMIT = 100
+#   Q2: Do they violate the Monolithic style?
+# LINES_PER_PAGE = 45
+# MAX_SIZE_LINE = 80
+# STOP_FREQUENCY_LIMIT = 100
 
 
 # Q3: Defining the main method ease testing, but does this count as violation of
 # the style?
-def main(file_path):
-    pass
+def main(file_path,
+         LINES_PER_PAGE = 45,
+         MAX_SIZE_LINE = 80,
+         STOP_FREQUENCY_LIMIT = 100
+         ):
+
+    # NOTE: we need to reset the value of all the variables because the unit tests do not restart the interpreter
+
+    # This contains the words that appear more than STOP_FREQUENCY_LIMIT times in the text.
+    # Q4: Maybe a set instead of list would have been a better choice?
+    stop_words = []
+
+    # The index that contains the words and the pages on which they appear.
+    # The idea is to store in the list 'data' objects (i.e., tuples) with the following content:
+    # data[0]:  - Word
+    # data[1]:  - Frequency
+    # data[2]:  - list of Page Numbers (without duplicate entries)
+    word_index = []
+
+    # We adopt an approach similar to C. Lopes:
+    # 1 - we iterate the file line by line
+    # 2 -     while doing this we keep track on which page (and line) we currently are and parse each word
+    # 3 -     when we parse a new word we store the page on which it appears and increment its frequency
+    # 4 -         if a word appears more than a number of time, it becomes automatically a stop word and must be eliminated/filtered out
+    # 5 - Before printing we sort words alphabetically (all the words are normalized lowercase)
+
+    line_index = 0
+    current_page = 0
+
+    # In this case, it is not necessary to append the '\n' at the end of the line
+    with open(file_path, 'r') as f:
+
+        for line in f:
+            # Q5: The line above could have been implemented as: "for line_index, line in enumerate(open(file_path))"
+            # Would have that violated the style? Why?
+
+            # If the line is non-terminated we will miss the last word...
+            if not line.endswith("\n"):
+                line += "\n"
+
+            # Compute the current page using the line_index. Pages START from 1
+            if line_index % LINES_PER_PAGE == 0:
+                current_page += 1
+
+            # Parse the words and update the various data structures.
+            # This code is similar to the one implementing the WordFrequency program
+
+            start_char = None
+            i = 0
+            for c in line:
+
+                if start_char is None:
+                    if c.isalnum():
+                        # We found the start of a word
+                        start_char = i
+                else:
+                    if not c.isalnum():
+                        # We found the end of a word. Process it
+                        found = False
+                        word = line[start_char:i].lower()
+
+                        # Ignore stop words:
+                        # Q6: Can the following 'not in' be considered a violation of the style? Why?
+                        # What would be an alternative implementation of this check?
+                        if word not in stop_words:
+                            # Let's see if the word already exists.
+                            # Q7: We loop over a COPY of the entire list. Why? Could using slices be considered a
+                            #   violation of the style?
+                            for data in word_index[:]:
+                                # data[0] is the word in lower case
+                                if word == data[0]:
+                                    found = True
+                                    # data[1] is the frequency
+                                    data[1] += 1
+                                    # Record Page Numbers. This must account for duplicates:
+                                    # Q8: Any violations here? What about using a set instead of a list?
+                                    if current_page not in data[2]:
+                                        data[2].append(current_page)
+
+                                    # Move word from word_index to stop_words
+                                    if data[1] > STOP_FREQUENCY_LIMIT:
+                                        stop_words.append(word)
+                                        word_index.remove(data)
+
+                                    break
+
+                            if not found:
+                                data = [word, 1, [current_page]]
+                                inserted = False
+                                index = 0
+
+                                # Ensure that every new word that we add is in the right place (alphabetic order)
+                                while index < len(word_index): # this time we iterate on the actual list and not a copy.
+                                    # However, we do not iterate over the values but by index.
+                                    if word < word_index[index][0]: # Check: https://thepythonguru.com/python-strings/
+                                        # Q9: Does using "string-comparison" count as a violation of the style?
+                                        word_index.insert(index, data)
+                                        inserted = True
+                                        break
+                                    index += 1
+
+                                # word is the last word
+                                if not inserted:
+                                    word_index.append(data)
+
+                        # Let's reset
+                        start_char = None
+
+                # Move "line-cursor" forward
+                i += 1
+
+            # Move "page-cursor" one line below
+            line_index += 1
+
+    for tf in word_index:
+        # Q10: Is this ('[1:-1]') a violation? What does it even mean ?!
+        print(tf[0], '-', str(tf[2])[1:-1])
 
 
 if __name__ == "__main__":
---
+++ session-02/task-01/word_index_unit_tests.py
@@ -0,0 +1,95 @@
+import unittest
+import io
+import inspect
+import os
+
+
+# Import the 'main function from word_index.py as wi_main
+from word_index import main as wi_main
+
+# We need to create three unit tests that make use of temp files/folders
+# https://simpleit.rocks/python/test-files-creating-a-temporal-directory-in-python-unittests/
+import tempfile
+
+# See: https://stackoverflow.com/questions/1218933/can-i-redirect-the-stdout-in-python-into-some-sort-of-string-buffer
+from contextlib import redirect_stdout
+
+
+class TestWordIndexUsingTempFiles(unittest.TestCase):
+
+    test_dir = None
+
+    # This is executed automatically before every test
+    def setUp(self):
+        # Create a temporary directory
+        self.test_dir = tempfile.TemporaryDirectory()
+
+    # This is executed automatically after every test
+    def tearDown(self):
+        # Remove the tempdir
+        self.test_dir.cleanup()
+
+    def test_program_with_no_lines(self):
+        # Print name of this test
+        print(inspect.stack()[0][3])
+
+        temp_file = os.path.join(self.test_dir.name, "empty.txt")
+
+        with open(temp_file, 'w') as f:
+            f.writelines("")
+
+        expected_output = "" #'\n'.join([""])
+
+        with io.StringIO() as buf, redirect_stdout(buf):
+            # Invoke main passing the temp file
+            wi_main(temp_file)
+            output = buf.getvalue()
+
+        print("OUTPUT IS")
+        print(output)
+
+        # Make the assertions
+        self.assertEqual(output, expected_output, msg="Output does not match")
+
+    def test_program_with_few_lines(self):
+        # Print name of this test
+        print(inspect.stack()[0][3])
+
+        temp_file = os.path.join(self.test_dir.name, "input.txt")
+
+        with open(temp_file, 'w') as f:
+            f.writelines('\n'.join(["foo", "bar"]))
+
+        expected_output = '\n'.join(["bar - 1", "foo - 1",""])
+
+        with io.StringIO() as buf, redirect_stdout(buf):
+            # Invoke main passing the temp file
+            wi_main(temp_file)
+            output = buf.getvalue()
+
+        # Make the assertions
+        self.assertEqual(output, expected_output, msg="Output does not match")
+
+    def test_that_overly_recurrent_words_are_not_reported(self):
+
+        # Print name of this test
+        print(inspect.stack()[0][3])
+
+        temp_file = os.path.join(self.test_dir.name, "input.txt")
+
+        with open(temp_file, 'w') as f:
+            f.writelines('\n'.join(["foo", "bar", "bar", "bar", "bar"]))
+
+        expected_output = '\n'.join(["foo - 1", ""])
+
+        with io.StringIO() as buf, redirect_stdout(buf):
+            # Invoke main passing the temp file.
+            wi_main(temp_file, STOP_FREQUENCY_LIMIT=3)
+
+            output = buf.getvalue()
+
+        # Make the assertions
+        self.assertEqual(output, expected_output, msg="Output does not match")
+
+if __name__ == '__main__':
+    unittest.main()
