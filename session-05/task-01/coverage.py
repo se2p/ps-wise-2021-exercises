17d5fac747c95b665db37957c6a3cda6c4f752f4
Alessio Gambi <alessio.gambi@uni-passau.de>
2020-12-10 20:40:17 +0100

    Solution to session 05

--- session-05/task-01/coverage.py
+++ session-05/task-01/coverage.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
-# Check those resources while preparing the solution of this exercise:
-# https://stackoverflow.com/questions/67631/how-to-import-a-module-given-the-full-path
-# https://stackoverflow.com/questions/1106840/find-functions-explicitly-defined-in-a-module-python
+import sys
+import inspect
+
+from importlib.machinery import SourceFileLoader
 
 #
 # The Introspective Style Description:
@@ -23,8 +24,201 @@
 #
 
 
-def main(args):
-    pass
+class Coverage:
+    """
+        This class implements a python ContextManager so it will automatically turned on and off at runtime.
+        To measure METHOD coverage we implement the approach described by Prof. A. Zeller in his book [1].
+
+        [1] https://www.fuzzingbook.org/html/Coverage.html
+    """
+
+    def __init__(self, imported_module):
+
+        self._imported_module = imported_module
+
+        # Compute basic properties of the imported_module using introspection
+
+        # Gather data about 'global' functions in the target module
+        list_of_functions = [o[0] for o in inspect.getmembers(self._imported_module)
+                                  if inspect.isfunction(o[1]) and inspect.getmodule(o[1]) == self._imported_module]
+
+        # Gather data about classes and methods in the target module.
+        list_of_methods = []
+        for class_name, class_obj in inspect.getmembers(self._imported_module, predicate=inspect.isclass):
+            for member_name, member_obj in inspect.getmembers(class_obj):
+                # Q: For some reason methods are not always tagged as such, any idea why?
+                if (inspect.isfunction(member_obj) or inspect.ismethod(member_obj))\
+                        and inspect.getmodule(member_obj) == self._imported_module:
+                    # Store the "fully qualified name" of the method
+                    fqn = ".".join([class_name, member_name])
+                    list_of_methods.append(fqn)
+
+        # Holds data about module's code
+        self._function_coverage = dict.fromkeys(list_of_functions, 0)
+        self._method_coverage = dict.fromkeys(list_of_methods, 0)
+
+        # Holds execution trace
+        self._trace = []
+
+    def _get_class_from_frame(self, fr):
+        """
+            Utility method to retrieve the class object from the trace object. Since this is not trivial at all
+            we assume the target program is written according to the "standard python style," so we check that
+            the first parameter of instance methods is named 'self' to identify instance-level class methods.
+        """
+        args, _, _, value_dict = inspect.getargvalues(fr)
+
+        if len(args) and args[0] == 'self':
+            # in that case, 'self' will be referenced in value_dict
+            instance = value_dict.get('self', None)
+            if instance:
+                # return its class
+                return getattr(instance, '__class__', None)
+        # return None otherwise
+        return None
+
+    def traceit(self, frame, event, arg):
+        """
+            Tracing method invocation
+        """
+
+        if self.original_trace_function is not None:
+            self.original_trace_function(frame, event, arg)
+
+        # Extract data from execution
+        function_name = frame.f_code.co_name
+        lineno = frame.f_lineno
+        # Utility method
+        class_obj = self._get_class_from_frame(frame)
+
+        # If you are curious to see the events that are generated during the execution
+        #   you can print them. Note that since we work by introspection and reflection,
+        #   using a debugger DOES NOT work here !
+        # Q: Can you guess why?
+        # print("TRACE", event, "->", frame.f_code.co_name, lineno, class_obj)
+
+        if event == "call":
+            self._trace.append((function_name, lineno))
+
+            # Calls to static functions or functions that are not associated to any object
+            if class_obj is None:
+                if function_name in self._function_coverage.keys():
+                    # Mark this function as covered
+                    self._function_coverage[function_name] = 1
+                return
+
+            if class_obj is not None:
+                class_name = class_obj.__name__
+                fqn = ".".join([class_name, function_name])
+                if fqn in self._method_coverage.keys():
+                    self._method_coverage[fqn] = 1
+
+        if event == "line":
+            self._trace.append((function_name, lineno))
+
+        # The trace function is invoked (with event set to 'call') whenever a new local scope is entered;
+        # it should return a reference to a local trace function to be used that scope, or None if the scope
+        # shouldn’t be traced."
+        # Here it returns itself, to handle all the tracing in the same function.
+        # A function is an object like anyone else, so there's no problem in returning itself.
+        # For example, it allows repeated calling on the same line:
+        #
+        # traceit("abc", "def", None)("ghi", "jkl", 3)("mno", "pqr", 4.3)
+        #
+        return self.traceit
+
+    # Start of `with` block
+    def __enter__(self):
+        """ Check https://www.geeksforgeeks.org/python-sys-settrace/"""
+        self.original_trace_function = sys.gettrace()
+        sys.settrace(self.traceit)
+        return self
+
+    # End of `with` block
+    def __exit__(self, exc_type, exc_value, tb):
+        sys.settrace(self.original_trace_function)
+
+    def trace(self):
+        """
+            The list of executed lines, as (function_name, line_number) pairs
+        """
+        return self._trace
+
+    def coverage(self):
+        """
+            The set of executed lines, as (function_name, line_number) pairs
+        """
+        return set(self.trace())
+
+    def print_coverage_summary(self):
+        """
+            The set of executed lines, as (function_name, line_number) pairs
+        """
+        total_functions = len(self._function_coverage.keys())
+        covered_functions = len([f for f in self._function_coverage.keys() if self._function_coverage[f] == 1])
+
+        total_methods = len(self._method_coverage.keys())
+        covered_methods = len([m for m in self._method_coverage.keys() if self._method_coverage[m] == 1])
+
+        print("COVERAGE SUMMARY")
+        if total_functions > 0:
+            print()
+            print("Covered", "".join([str((covered_functions/total_functions)*100), "%"]), "out of", total_functions, "functions")
+            for name in self._function_coverage.keys():
+                if self._function_coverage[name] == 1:
+                    print("# ", end="")
+                else:
+                    print("  ", end="")
+                print(name)
+            print()
+
+        if total_methods > 0:
+            print()
+            print("Covered", "".join([str((covered_methods / total_methods) * 100), "%"]), "out of", total_methods, "methods")
+            for method_name in self._method_coverage.keys():
+                if self._method_coverage[method_name] == 1:
+                    print("# ", end="")
+                else:
+                    print("  ", end="")
+                print(method_name)
+            print()
+
+    def annotated_code(self):
+        # decode_code is a string holding the source code.
+        # We can later print it out with Python syntax highlighting:
+        decode_code = inspect.getsource(self._imported_module)
+        decode_lines = [""] + decode_code.splitlines()
+        covered_lines = [line[1] for line in self.coverage()]
+
+        for lineno in range(1, len(decode_lines)):
+            prefix ="  "
+            if lineno in covered_lines:
+                prefix = "* "
+
+            decode_lines[lineno] = "".join([prefix, str(lineno).zfill(3), " ", decode_lines[lineno]])
+        return decode_lines
+
+
+def main(arguments):
+
+    word_index = arguments[1]
+    input_file = arguments[2]
+
+    # Import the module given in input
+    # Note: We work under the assumption that the module has a "main(args)" function
+    imported_module = SourceFileLoader("module.name", word_index ).load_module()
+
+    # Execute the main method in the module, and compute coverage. Method coverage
+    #   data are stored inside the coverage object
+    with Coverage(imported_module) as coverage:
+        imported_module.main(input_file)
+
+    # Plot coverage information:
+    coverage.print_coverage_summary()
+
+    # Print the content of source file indicating the covered elements
+    for line in coverage.annotated_code():
+        print(line)
 
 
 if __name__ == "__main__":
--- session-05/task-01/word_index.py
+++ session-05/task-01/word_index.py
@@ -1,12 +1,103 @@
 #!/usr/bin/env python
+import sys, re
+import os
 
+# The Things Style Description:
 #
-# TODO: Copy here the Word Index program that you implemented in the Things style
+# - The larger problem is decomposed into 'things' that make sense for
+#   the problem domain
 #
+# - Each 'thing' is a capsule of data that exposes procedures to the
+#   rest of the world
+#
+# - Data is never accessed directly, only through these procedures
+#
+# - Capsules can reappropriate procedures defined in other capsules
+
+class Configuration:
+    _PAGE_SIZE = 45
+    _FREQ_LIMIT = 100
+
+    def get_page_size(self):
+        return self._PAGE_SIZE
+
+    def get_freq_limit(self):
+        return self._FREQ_LIMIT
+
+
+class DataStorageManager:
+    """ Models the contents of the file """
+
+    # Cursor. Arrays start at 0 but lines on a page start at 1
+    _currentLine = 0
+
+    pattern = re.compile('[\W_]+')
+
+    def __init__(self, path_to_file):
+        with open(path_to_file) as f:
+            # Q: Is this correct ?
+            self._data = f.read()
+        self._lines = self._data.split('\n')
+        for idx in range(len(self._lines)):
+            self._lines[idx] = self.pattern.sub(' ', self._lines[idx]).lower()
+
+    def has_next_line(self):
+        return self._currentLine < len(self._lines) - 1
+
+    def next_line(self):
+        """ Return the words in the line or raise an exception """
+        self._currentLine += 1
+        return self._currentLine, self._lines[self._currentLine - 1].split()
+
+
+class WordFrequencyManager:
+
+    def __init__(self):
+        self._word_freqs = {}
+
+    def increment_count(self, word, page):
+        if word in self._word_freqs:
+            self._word_freqs[word][1].append(page)
+            self._word_freqs[word] = \
+                (self._word_freqs[word][0] + 1,
+                 # Avoid duplicates
+                 list(set(self._word_freqs[word][1])))
+        else:
+            self._word_freqs[word] = (1, [page])
+
+    def filter_and_sort(self, limit):
+        # Filtering by frequency
+        self._word_freqs = {k: v for k, v in self._word_freqs.items()
+                            if v[0] <= limit} # ??
+        # Sorting
+        return sorted(self._word_freqs.items())
+
+
+class WordFrequencyController:
+
+    def __init__(self, path_to_file):
+        self._storage_manager = DataStorageManager(path_to_file)
+        self._conf = Configuration()
+        self._word_freq_manager = WordFrequencyManager()
+
+    def run(self):
+        while self._storage_manager.has_next_line():
+            line_number, words = self._storage_manager.next_line()
+            page_number = int(line_number / self._conf.get_page_size() ) + 1
+            for word in words:
+                self._word_freq_manager.increment_count(word, page_number)
+
+        word_freqs = self._word_freq_manager.filter_and_sort(self._conf.get_freq_limit())
+
+        for tf in word_freqs:
+            print(tf[0], '-', str(tf[1][1])[1:-1])
 
 
 def main(file_path):
-    pass
+    if os.path.exists(file_path):
+        wfc = WordFrequencyController(file_path)
+        wfc.run()
+
 
 if __name__ == "__main__":
     main(sys.argv[1])
